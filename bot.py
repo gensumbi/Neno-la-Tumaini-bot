130cf20eafd0f56605025b0e43b3b5a416ddb970
gensumbi <gensumbi@gmail.com>
2026-09-14 18:31:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,20 +2,26 @@ from flask import Flask, request
 import os
 
 app = Flask(__name__)
-VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN", "neno_tumaini_2026")
+VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN")
 
-@app.route("/webhook", methods=['GET'])
+@app.route('/', methods=['GET'])
+def home():
+    return "Bot ya NENO LA TUMAINI iko Live ✅", 200
+
+@app.route("/webhook", methods=['GET', 'POST'])
 def verify():
-    token = request.args.get("hub.verify_token")
-    challenge = request.args.get("hub.challenge")
+    if request.method == 'GET':
+        token = request.args.get("hub.verify_token")
+        challenge = request.args.get("hub.challenge")
+        if token == VERIFY_TOKEN:
+            return challenge, 200
+        else:
+            return "Verification failed", 403
     
-    if token == VERIFY_TOKEN:
-        return challenge
-    return "Invalid token", 403
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data) # hii itatusaidia kuona ujumbe ukiingia
+        return "ok", 200
 
-@app.route("/webhook", methods=['POST'])
-def webhook():
-    return "ok", 200
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
